7aa56fa2ea1e2e6b85d5f44ab6919fdf65b1d17f
Vitor Reis <vitor.reis@izap.com.br>
2023-12-15 05:45:23 -0300

    refactor: port by env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const configService = new ConfigService();
+
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({ origin: '*' });
 
-  await app.listen(3000);
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
